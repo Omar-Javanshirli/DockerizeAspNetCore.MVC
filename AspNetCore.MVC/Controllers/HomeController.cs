3c3b7c0171c7edbcb68c12f336acc0e68e3da0f5
javanshirli_o@itstep.academy <javanshirli_o@itstep.academy>
2023-06-09 06:23:51 -0700

    Add project files.

---
+++ AspNetCore.MVC/Controllers/HomeController.cs
@@ -0,0 +1,82 @@
+﻿using AspNetCore.MVC.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.FileProviders;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AspNetCore.MVC.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+        private readonly IFileProvider fileProvider;
+        public HomeController(ILogger<HomeController> logger, IFileProvider fileProvider)
+        {
+            _logger = logger;
+            this.fileProvider = fileProvider;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult ImageShow()
+        {
+            var images = this.fileProvider.GetDirectoryContents("wwwroot/Images")
+                .ToList().Select(x => x.Name);
+
+            return View(images);
+        }
+
+        [HttpPost]
+        public IActionResult ImageShow(string name)
+        {
+            var file = this.fileProvider.GetDirectoryContents("wwwroot/Images")
+                .ToList().First(x => x.Name == name);
+
+            System.IO.File.Delete(file.PhysicalPath);
+            return RedirectToAction(nameof(ImageShow));
+        }
+        public IActionResult ImageSave()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> ImageSave(IFormFile imageFile)
+        {
+            if (imageFile is { Length: > 0 } && imageFile != null)
+            {
+                var fileName = Guid.NewGuid().ToString() +
+                    Path.GetExtension(imageFile.FileName);
+
+                var path = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot/Images", fileName);
+
+                using (var stream = new FileStream(path, FileMode.Create))
+                {
+                    await imageFile.CopyToAsync(stream);
+                }
+            }
+            return View();
+        }   
+
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
